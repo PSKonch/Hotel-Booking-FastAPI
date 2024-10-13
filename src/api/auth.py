f12ffa9fc739fcb1e4d6d03c9dd7142cb5755797
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-13 14:26:50 +0300

    uniq-email

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Body, Query
+from fastapi import APIRouter, Body, Query, HTTPException, status
 
 from passlib.context import CryptContext
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import User, UserAdd, UserRequestAdd
+from src.models.users import UsersModel
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -13,8 +14,12 @@ router = APIRouter(prefix='/auth', tags=['Авторизация и Аутент
 @router.post('/register')
 async def register_user(data: UserRequestAdd):
     hashed_password = pwd_context.hash(data.password)
+
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
+        email_is_used = await UsersRepository(session).is_email_used(data.email)
+        if email_is_used:
+            return {'error': 'email is required'}
         await UsersRepository(session).add(new_user_data)
         await session.commit()
     
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,8 +1,17 @@
+from sqlalchemy import select, Boolean
+
 from src.repositories.base import BaseRepository
 
 from src.models.users import UsersModel
 from src.schemas.users import User, UserAdd
 
 class UsersRepository(BaseRepository):
-    model = UsersModel
+    model = UsersModel  
     schema = User
+
+    async def is_email_used(self, email: str) -> bool:
+        query = select(self.model).where(self.model.email==email)
+        result = await self.session.execute(query)
+        response = result.scalars().first()
+        return response is not None
+        
