56952668031a4600212536e3dd84473fb7976619
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-13 18:12:23 +0300

    refactoring-auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -6,32 +6,16 @@ from passlib.context import CryptContext
 import jwt
 
 from src.database import async_session_maker
+from src.services.auth import AuthService
 from src.repositories.users import UsersRepository
 from src.schemas.users import User, UserAdd, UserRequestAdd
 from src.models.users import UsersModel
 
-SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
 router = APIRouter(prefix='/auth', tags=['Авторизация и Аутентификация'])
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
 
 @router.post('/register')
 async def register_user(data: UserRequestAdd):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
 
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
@@ -50,10 +34,10 @@ async def login_user(data: UserRequestAdd, resonse: Response):
         if not user:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Пользователь не существует')
         
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Неверный пароль')
         
-        access_token = create_access_token({'user_id': user.id})
+        access_token = AuthService().create_access_token(({'user_id': user.id}))
         resonse.set_cookie('access_token', access_token)
 
         return {'access_toket': access_token}
