78717dc1dcc03ea31c7c2e8b7bb705004512dc34
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-15 19:37:02 +0300

    bookings-model

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -11,6 +11,7 @@ from src.database import Base
 from src.models.hotels import HotelsModel
 from src.models.rooms import RoomsModel
 from src.models.users import UsersModel
+from src.models.bookings import BookingsModel
 
 from src.config import settings
 
