74b68284ff66aa340a5ee9c5a2fa396eff054592
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2024-10-28 01:45:29 +0300

    celery-beat-schedule

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,6 +1,10 @@
+import asyncio
 from time import sleep
 from PIL import Image
+
 from src.tasks.celery_app import celery_instance
+from src.utils.db_manager import DBManager
+from src.database import async_session_maker_null_pool
 
 import os
 
@@ -32,3 +36,13 @@ def resize_image(image_path: str):
         img_resized.save(output_path)
         
     print(f"Изображение сохранено в следующих размерах: {sizes} в папке {output_folder}")
+
+async def get_bookings_with_today_checkin_helper():
+    print("Я ЗАПУСКАЮСЬ")
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        bookings = await db.bookings.get_bookings_with_today_checkin()
+        print(f"{bookings=}")
+@celery_instance.task(name="booking_today_checkin")
+
+def send_emails_to_users_with_today_checkin():
+    asyncio.run(get_bookings_with_today_checkin_helper())
